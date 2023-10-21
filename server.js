fe4b87b71065f5b791ea16e2f99d63bd9143b235
Nimish-Dureja-10 <durejanimish14@gmail.com>
2023-10-21 23:03:09 +0530

    .

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ app.use("/api/v1/category",categoryRoutes);
 app.use('/api/v1/products',productRoutes);
 
 //rest api
-app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"));
-});
+// app.use("*",function(req,res){
+//     res.sendFile(path.join(__dirname,"./client/build/index.html"));
+// });
 
 const PORT = process.env.PORT || 8080;
 
